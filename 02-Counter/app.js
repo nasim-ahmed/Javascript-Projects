38f05b5e038eebe3b242fc7248c785aec7e79eff
nasim-ahmed <nasim.ahmed54@gmail.com>
2022-03-19 11:35:24 +0100

    Added counter project

---
+++ 02-Counter/app.js
@@ -0,0 +1,28 @@
+let count = 0 
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+         const className = e.currentTarget.classList;
+         if (className.contains("decrease")){
+             count --;
+         }else if(className.contains("increase")){
+            count ++;
+         }else{
+             count = 0;
+         }
+         if(count > 0){
+             value.style.color = "green"
+         }
+         if(count < 0){
+            value.style.color = "red"
+         }
+         if(count === 0) {
+            value.style.color = "#222"
+         }
+
+         value.textContent = count;
+    })
+})
